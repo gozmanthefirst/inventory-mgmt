0fa5c7cf9f51f3893c09b16fe0222f8a51994b26
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-10-04 00:29:17 +0100

    update middlewares

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,11 +1,22 @@
 // External Imports
 import { ErrorRequestHandler } from "express";
 
+// Local Imports
+import { HttpError } from "../interfaces/httpError";
+import { errorResponse } from "../utils/api-response";
+
 /**
  * Handles all errors thrown in the app.
  */
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  res.status(err.status ?? 500).json({ error: err.message });
+  console.error(err.stack);
+  res
+    .status(err.status ?? 500)
+    .json(
+      err.status === 500
+        ? errorResponse("SERVER_ERROR", "Internal Server Error")
+        : errorResponse((err as HttpError)?.code!, (err as HttpError)?.details!)
+    );
 };
 
 export default errorHandler;
