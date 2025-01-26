effa9f816ae78f93624161dc5511df96a909f5b2
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-01-26 17:58:59 +0100

    fix: fix wrong link in 'app running' console message

--- src/index.ts
+++ src/index.ts
@@ -51,5 +51,5 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost/${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
